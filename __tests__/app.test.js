7975c9c831efec00f3da8902f54a8b7ff3759d3d
KDSigel <heykarl@gmail.com>
2022-02-10 10:34:20 -0800

    added mock file from past lab and added blank test for checking signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,11 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const fakeUser = {
+  email: 'broken@arrow.com',
+  password: 'Nucflash'
+};
+
 describe('backend routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -11,4 +16,14 @@ describe('backend routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('Users can sign up using their Github account', () => {
+  // a get request that redirects user to github sign in
+  // gets back code to user for request to user info?
+  // 
+  });
+
+
+
+
 });
